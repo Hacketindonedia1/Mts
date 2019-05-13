7018b219a755f81a626d534a2675df420426e53e
Hacketindonedia1 <49530313+Hacketindonedia1@users.noreply.github.com>
2019-05-13 20:30:14 +0700

    New upload
    Selamat datang di tools Mts

---
+++ mts.sh
@@ -0,0 +1,489 @@
+#!/system/xbin/bash
+#RecodeGabakalBikinLuPro
+
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+
+echo  $white
+echo "Loading";
+sleep 0.1;
+clear
+
+echo
+echo  $green
+echo "10%";
+sleep 0.1;
+clear
+
+echo  $green
+echo "45%";
+sleep 0.1;
+clear
+
+echo  $green
+echo "85%";
+sleep 0.1;
+clear
+
+echo  $green
+echo "100%[Done]";
+sleep 0.1;
+clear 
+
+echo  ~"$red"{"$white"Welcome to tools mts:v"$cyan"}
+echo  ~"$green"{"$white"Tools masih V.1 jadi maklum kalo masih dikit"$cyan"}"$green"
+echo  $white
+echo "Gunakan Dengan Baik Oke:v";
+sleep 0.1;
+echo  $white
+echo "Loading.......";
+sleep 0.1;
+echo  $white
+echo "[Done]";
+sleep 0.1;
+
+echo
+echo $blue"==============================================="
+echo $indigo"	~"$cyan"{"$white"Author"$red"}"$green":"$yellow"Hacker online"$cyan
+echo $indigo"    ~"$cyan"{"$white"Whatsapp"$blue"}"$green":"$yellow"089655478810"$cyan
+echo $indigo"    ~"$cyan"{"$blue"Email"$cyan"}"$green":"$yellow"agunggebang1@gmail.com"$blue
+echo $blue"==============================================="
+echo
+echo $red"==============================================="
+echo  $white
+echo "[Silahkan Pilih Tools mtsV1Nya]";
+sleep 0.1;
+echo $green "[Author : ~|Frezz|~ <>Whatsapp : 089655478810 <>Email : agunggebang1@gmail.com]"
+echo $red"==============================================="
+echo
+echo $green"==============================================="
+echo $white "[1]»Spam Akun Email"
+echo $green"==============================================="
+echo $blue"==============================================="
+echo $red "[2]»Spam MultiSpam sms"
+echo $blue"==============================================="
+echo $red"==============================================="
+echo $cyan "[3]»Virus Maker"
+echo $red"==============================================="
+echo $yellow"==============================================="
+echo $purple "[4]»Bikin Script Deface"
+echo $yellow"==============================================="
+echo $white"==============================================="
+echo $blue "[5]»Install Red_Hawk [Buat Scan Web]"
+echo $white"==============================================="
+echo $blue"==============================================="
+echo $green "[6]»Install LiteDDOS [Buat DDOS web]"
+echo $blue"==============================================="
+echo $red"==============================================="
+echo $white "[7]»Menampilkan Animasi Kereta:v"
+echo $red"==============================================="
+echo $white"==============================================="
+echo $green "[8]»Menampilkan Kode2 [Biar Kaya Hacker Pro:v]"
+echo $white"==============================================="
+echo $cyan"==============================================="
+echo $blue "[9]»Menampilkan Informasi Android"
+echo $cyan"==============================================="
+echo $purple"==============================================="
+echo $yellow "[10]»Install IPGeoLocation Buat Lacak IP"
+echo $purple"==============================================="
+echo $yellow"==============================================="
+echo $red "[11]»Install Nethunter"
+echo $yellow"==============================================="
+echo $green"==============================================="
+echo $blue "[12]»Install Weeman [Buat Phising]"
+echo $green"==============================================="
+echo $purple"==============================================="
+echo $cyan "[13]»Hack Fb Masal"
+echo $purple"==============================================="
+echo $white"==============================================="
+echo $green "[14]»Bobol Wifi"
+echo $white"==============================================="
+echo $green"==============================================="
+echo $yellow "[15]»Menampilkan Ip"
+echo $green"==============================================="
+echo $red"==============================================="
+echo $indigo "[16]»Install Websploit"
+echo $red"==============================================="
+echo $cyan"==============================================="
+echo $blue "[17]»Install Layzmux"
+echo $cyan"==============================================="
+echo $white"==============================================="
+echo $green "[18]»Install Google Dork"
+echo $white"==============================================="
+echo $yellow"==============================================="
+echo $indigo "[19]»Install Zambie"
+echo $yellow"==============================================="
+echo $red"==============================================="
+echo $blue "[20]»Spam Whatsapp"
+echo $red"==============================================="
+echo $white"==============================================="
+echo $green "[21]»Free Kouta"
+echo $white"==============================================="
+echo $purple"==============================================="
+echo $blue "[22]»Browsing Di Termux"
+echo $purple"==============================================="
+echo $indigo"==============================================="
+echo $red "[23]»Youtube Di Termux"
+echo $indigo"==============================================="
+echo $yellow"==============================================="
+echo $blue "[24]»Tutorial Termux By Frezz"
+echo $yellow"==============================================="
+echo $green"==============================================="
+echo $white "[25]»Install <>VI<>SQLScan"
+echo $green"==============================================="
+echo $indigo"|Hacker|Hacker|Hacker|Hacker|Hacker|Hacker|"
+echo $red "[00]»Keluar"
+echo $indigo"|hacker|hacker|hacker|hacker|hacker|hacker|"
+echo $green "[Warning]Pilih Yg mau lu Install ea:v"
+echo $red "Author : Hacker online | Whatsapp : 089655478810 | email : agunggebang1@gmail.com"
+echo $blue "Pm Terus Biar Terkenal:v"
+echo
+echo "\033[32;1m"
+read -p "@Hacker ~#" pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update
+pkg install git 
+pkg install python2
+git clone https://github.com/root-ID-041/bom-mail
+ls
+cd bom-mail
+ls
+python2 Bom-mail.py
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg install php
+pkg install git 
+pkg install toilet 
+git clone https://github.com/4L13199/LITESPAM
+ls
+cd LITESPAM
+ls
+sh LITESPAM.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Junior60/vbug
+ls
+cd vbug
+ls
+python2 vbug.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITESCRIPT
+ls
+cd LITESCRIPT
+ls
+python2 LITESCRIPT.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg install curl
+pkg install php
+pkg install git
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+ls
+cd RED_HAWK
+ls
+php rhawk.php
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/4L13199/LITEDDOS
+ls
+cd LITEDDOS
+ls
+python2 liteDDOS.py  
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install sl
+sl
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install matrix
+matrix
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install neofetch
+neofetch
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/maldevel/IPGeoLocation
+ls
+cd IPGeolocation
+ls
+chmod 775 ipgeolocation.py
+pip install -r requirements.txt
+python ipgeolocation.py -m
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Hax4us/Nethunter-In-Termux.git
+ls
+cd Nethunter
+Is
+chmod +x kalinethunter
+./kalinethunter
+./startkali.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+ git clone https://github.com/evait-security/weeman.git
+ls
+cd weeman
+ls
+python2 weeman.py
+fi 
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+pip2 install mechanize
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook
+ls
+cd mbf
+ls
+python2 MBF.py
+fi 
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+ls
+cd wifi-hacker
+ls
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+ifconfig
+fi
+
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg install git
+pip2 install scapy
+git clone https://github.com/The404Hacking/websploit.git
+ls
+cd websploit
+ls
+chmod 777 *.py
+python2 websploit.py
+fi
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install git
+pkg install python2
+pkg install clang
+git clone https://github.com/Gameye98/Lazymux 
+ls
+chmod +x meta-install.sh && ./meta-install.sh
+fi
+
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/CiKu370/ko-dork
+ls
+cd ko-dork
+ls
+python2 dork.py
+fi
+
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/zanyarjamal/zambie
+ls
+fi
+
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/siputra12/prank
+ls
+cd prank
+ls
+php wa.php 
+fi
+
+if [ $pil = 21 ]
+then
+clear
+figlet -f slant "T E R T I P U!!"|lolcat
+echo $red "[TERTIPU WKWKWK]"
+fi
+
+if [ $pil = 22 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install lynx
+lynx www.google.com
+fi
+
+if [ $pil = 23 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python
+pkg install mpv
+pip install mps_youtube
+pip install youtube_dl
+mpsyt
+search lingsir wengi.mp3
+fi
+
+if [ $pil = 24 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+echo $indigo "[Chat Whatsapp : 089655478810]"
+fi
+
+if [ $pil = 25 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+pkg update&&pkg upgrade
+pkg install python
+git clone https://github.com/blackvkng/viSQL
+ls
+cd viSQL
+ls
+chmod 777 viSQL.py
+python2 viSQL.py -h
+fi 
+
+if [ $pil = 00 ]
+then
+clear
+figlet -f slant "F R E Z Z"|lolcat
+echo
+echo  $green
+echo "We Are Legion";
+sleep 0.1;
+clear
+echo
+echo  $green
+echo "We Are Noob";
+sleep 0.1;
+clear
+echo
+echo  $green
+echo "But We Are Handsome";
+sleep 0.1;
+clear
+echo
+echo  $green
+echo "Expect Us";
+sleep 0.1;
+clear
+echo
+echo  $green
+echo "FrezzGans";
+sleep 0.1;
+clear
+fi
